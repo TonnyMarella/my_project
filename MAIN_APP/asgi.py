8b12cdd3b8d2c01df6206fee5228a87595f1b20b
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2022-09-07 21:57:59 +0300

    refactor files

--- MAIN_APP/asgi.py
+++ MAIN_APP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for my_project1 project.
+ASGI config for MAIN_APP project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MAIN_APP.settings')
 
 application = get_asgi_application()
