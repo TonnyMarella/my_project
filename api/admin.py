a7735d60f55d638a9848972e860bc478aaf9fe6e
KapOo <temazubkov02@gmail.com>
2022-09-09 18:02:54 +0300

    Changed

--- api/admin.py
+++ api/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from api.models import Task
 
 
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'exercise', 'author', 'executor', 'status', 'created_at', 'modified_at')
     list_display_links = ('title',)
     search_fields = ('title', 'author', 'executor')
     list_editable = ('status',)
     list_filter = ('created_at', 'modified_at')
-
-
-admin.site.register(Task, TaskAdmin)
