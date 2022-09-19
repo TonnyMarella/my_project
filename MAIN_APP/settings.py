a01c321e7aa3668bbb6baf34818e929cd045edcc
KapOo <temazubkov02@gmail.com>
2022-09-19 23:08:10 +0300

    flake8

--- MAIN_APP/settings.py
+++ MAIN_APP/settings.py
@@ -158,8 +158,8 @@ REST_FRAMEWORK = {
     ),
 }
 
-CELERY_BROKER_URL = "redis://redis:6379"
-CELERY_RESULT_BACKEND = "redis://redis:6379"
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
